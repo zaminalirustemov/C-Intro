b3d39fad6e64d11583cf505ac1c818d8de498d9b
zaminalirustemov <2g17rck@code.edu.az>
2022-11-16 09:44:53 +0400

    homework

---
+++ Task1/Task1/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            Console.WriteLine("Ilk ededi daxil edin:");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ikinci ededi daxil edin:");
+            num2 = Convert.ToInt32(Console.ReadLine()); 
+
+            for (int i = num1; i < num2; i++)
+            {
+                if (i%2!=0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Task2/Task2/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Code Academy";
+            word = word.Replace(" ", string.Empty);
+            word = word.Replace("C", "c");
+            word = word.Replace("A", "a");
+            while (word.Length > 0)
+            {
+                Console.Write(word[0] + " = ");
+                int count = 0;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[0] == word[i])
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+                word = word.Replace(word[0].ToString(), string.Empty);
+            }
+        }
+    }
+}
